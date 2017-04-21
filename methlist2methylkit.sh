bf15c3813f388635284450ab11f3f9e9b6ceeb34
Graham Gower <graham.gower@gmail.com>
2017-04-21 16:46:16 +0930

    Output separate forward and reverse strands for CpG and CHG contexts.

--- methlist2methylkit.sh
+++ methlist2methylkit.sh
@@ -19,24 +19,38 @@ BEGIN {
 }
 
 $4=="CpG" {
-  dp = $5+$6+$7+$8;
-  printf "%s.%s\t%s\t%d\t+\t%d\t%.2f\t%.2f\n",
-  	 $1, $2, $1, $2+1, dp, 100*($6+$8)/dp, 100*($5+$7)/dp >> pfx".CpG.txt"
-}
-$4=="CHG" {
-  dp = $5+$6+$7+$8;
-  printf "%s.%s\t%s\t%d\t+\t%d\t%.2f\t%.2f\n",
-  	 $1, $2, $1, $2+1, dp, 100*($6+$8)/dp, 100*($5+$7)/dp >> pfx".CHG.txt"
-}
-$4=="CHH" {
-  f_dp = $5+$6;
+  f_dp = $5+$6
   r_dp = $7+$8
   if (f_dp) {
-    printf "%s.%s\t%s\t%d\t+\t%d\t%.2f\t%.2f\n",
-  	 $1, $2, $1, $2+1, f_dp, 100*$6/f_dp, 100*$5/f_dp >> pfx".CHH.txt"
+    printf "%s.%s\t%s\t%d\tF\t%d\t%.2f\t%.2f\n",
+           $1, $2+1, $1, $2+1, f_dp, 100*$6/f_dp, 100*$5/f_dp >> pfx".CpG.txt"
+  }
+  if (r_dp) {
+    printf "%s.%s\t%s\t%d\tR\t%d\t%.2f\t%.2f\n",
+           $1, $2+2, $1, $2+2, r_dp, 100*$8/r_dp, 100*$7/r_dp >> pfx".CpG.txt"
+  }
+}
+$4=="CHG" {
+  f_dp = $5+$6
+  r_dp = $7+$8
+  if (f_dp) {
+    printf "%s.%s\t%s\t%d\tF\t%d\t%.2f\t%.2f\n",
+           $1, $2+1, $1, $2+1, f_dp, 100*$6/f_dp, 100*$5/f_dp >> pfx".CHG.txt"
+  }
+  if (r_dp) {
+    printf "%s.%s\t%s\t%d\tR\t%d\t%.2f\t%.2f\n",
+           $1, $2+3, $1, $2+3, r_dp, 100*$8/r_dp, 100*$7/r_dp >> pfx".CHG.txt"
+  }
+}
+$4=="CHH" {
+  f_dp = $5+$6
+  r_dp = $7+$8
+  if (f_dp) {
+    printf "%s.%s\t%s\t%d\tF\t%d\t%.2f\t%.2f\n",
+           $1, $2+1, $1, $2+1, f_dp, 100*$6/f_dp, 100*$5/f_dp >> pfx".CHH.txt"
   } else if (r_dp) {
-    printf "%s.%s\t%s\t%d\t-\t%d\t%.2f\t%.2f\n",
-  	 $1, $2, $1, $2+1, r_dp, 100*$8/r_dp, 100*$7/r_dp >> pfx".CHH.txt"
+    printf "%s.%s\t%s\t%d\tR\t%d\t%.2f\t%.2f\n",
+           $1, $2+1, $1, $2+1, r_dp, 100*$8/r_dp, 100*$7/r_dp >> pfx".CHH.txt"
   }
 }
 ' - $@
