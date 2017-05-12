0ca3d26a2991bb9b6c1632e0935675e41bca8e64
Graham Gower <graham.gower@gmail.com>
2017-05-12 12:25:19 +0930

    Missed files last commit.

--- Makefile
+++ Makefile
@@ -3,16 +3,19 @@ CFLAGS=-I$(HTSDIR) -Wall -g -O2
 LDFLAGS=-L$(HTSDIR)
 LIBS= #-Wl,-Bstatic -lhts -lz -Wl,-Bdynamic -lm
 
-all: foldreads mark_5mC scan_pairs
+all: foldreads foldreads2 mark_5mC scan_pairs
+
+foldreads2: foldreads2.o
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS) -Wl,-Bstatic -lz -Wl,-Bdynamic -lm
 
 foldreads: foldreads.o
-	$(CC) $(CFLAGS) $< -o $@ $(LDFLAGS) -Wl,-Bstatic -lz -Wl,-Bdynamic -lm
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS) -Wl,-Bstatic -lz -Wl,-Bdynamic -lm
 
-mark_5mC: mark_5mC.o
-	$(CC) $(CFLAGS) $< -o $@ $(LDFLAGS) -Wl,-Bstatic -lhts -lz -Wl,-Bdynamic -pthread
+mark_5mC: mark_5mC.o fold.o
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS) -Wl,-Bstatic -lhts -lz -Wl,-Bdynamic -pthread -lm
 
-scan_pairs: scan_pairs.o
-	$(CC) $(CFLAGS) $< -o $@ $(LDFLAGS) -Wl,-Bstatic -lhts -lz -Wl,-Bdynamic -pthread
+scan_pairs: scan_pairs.o fold.o
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS) -Wl,-Bstatic -lhts -lz -Wl,-Bdynamic -pthread -lm
 
 clean:
-	rm -f *.o foldreads mark_5mC scan_pairs
+	rm -f *.o foldreads foldreads2 mark_5mC scan_pairs
---
+++ fold.h
@@ -0,0 +1,52 @@
+#ifndef _FOLD_H
+#define _FOLD_H
+
+#define AVG_ERR 0.02
+#define MAXDIFF_THRES 0.01
+#define MAXLEN 1000
+
+int
+maxdiff(int l, double err, double thres);
+
+void
+revcomp(char *s, size_t len);
+
+void
+reverse(char *s, size_t len);
+
+int
+find_hp_adapter(const char *s1, size_t len1,
+		const char *s2, size_t len2,
+		const char *hairpin, const char *rhairpin,
+		size_t hlen,
+		const char *adapter1, const char *adapter2,
+		size_t a1len, size_t a2len,
+		size_t adapter_matchlen,
+		int *h1, int *h2,
+		int *a1, int *a2);
+
+int
+match2(const char *s1, const char *q1, size_t len1,
+	const char *s2, const char *q2, size_t len2,
+	char *s_out, char *q_out,
+	int allow_bs,
+	int phred_scale_in, int phred_scale_out);
+
+int
+match4(const char *_s1, const char *_q1, size_t len1,
+	const char *_s2, const char *_q2, size_t len2,
+	const char *_s3, const char *_q3, size_t len3,
+	const char *_s4, const char *_q4, size_t len4,
+	char *s_out, char *q_out,
+	int phred_scale_in, int phred_scale_out);
+
+int
+correct_s1s2(char *s1, char *q1, size_t len1,
+		char *s2, char *q2, size_t len2,
+		const char *hairpin, const char *rhairpin, size_t hlen,
+		int phred_scale_in, int phred_scale_out);
+
+int
+xf2ssqq(char *xf, char **s1, char **s2, char **q1, char **q2);
+
+#endif // _FOLD_H
